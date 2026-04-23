dbb5557e015ae6baee4f16de9ad0dc4d87002359
Diego Barcellos <dcastrob@emeal.nttdata.com>
2026-04-23 00:29:21 -0300

    Desafio 1 da Aula 2 - Calculadora de IMC

---
+++ src/main/java/aula2/Desafio1CalculoImc.java
@@ -0,0 +1,42 @@
+package main.java.aula2;
+
+/*
+Cálculo do IMC
+
+Desenvolva um programa que calcule o Índice de Massa Corporal (IMC) de uma pessoa. O programa deve:
+Ler o nome, peso (em kg) e altura (em metros)
+Calcular o IMC usando a fórmula: IMC = peso / (altura²)
+Exibir o nome da pessoa e seu IMC
+Entrada: Nome (String), peso (double), altura (double) Saída: Mensagem com nome e IMC
+ */
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Desafio1CalculoImc {
+
+    public static void main(String[] args) {
+        String nome;
+        double peso;
+        double altura;
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite seu nome: ");
+        nome = sc.nextLine();
+
+        System.out.println("Digite seu peso: ");
+        peso = sc.nextDouble();
+
+        System.out.println("Digite sua altura: ");
+        altura = sc.nextDouble();
+
+        double imc = (peso / (altura * altura));
+
+        System.out.printf("Olá " + nome + " o seu IMC é: %.2f%n", imc);
+
+        sc.close();
+    }
+
+}
